734f4c3bad8ac09dd213e71b491a40e15dce4a02
mt <maxterskov@gmail.com>
2020-02-27 03:39:34 +0300

    map generate, coefBenefit at goods, isPossibleToLoadGoods & isPossibleToSellGoods, whereAmI, test funcs nextDestination & wayToPort & direction

--- client/game.mjs
+++ client/game.mjs
@@ -1,60 +1,185 @@
-const TOTAL_VOLUME = 369
+const TOTAL_VOLUME = 368
 let AVAILABLE_VOLUME = TOTAL_VOLUME
-let MAP = ''
+let MAP = []
+let NEW_GOODS_ARR = []
 
 export function startGame(levelMap, gameState) {
-	MAP = levelMap
 	AVAILABLE_VOLUME = TOTAL_VOLUME
+	generateMap(levelMap, gameState)
+	cofficentOfBenefit(gameState)
 }
 
 
 export function getNextCommand(gameState) {
-	console.log(gameState)
 
+	console.log('startGame: ', gameState)
 	setAvailableVolume(gameState)
-	console.log(AVAILABLE_VOLUME)
 
-	if (isPort(gameState) === 'isHome' && AVAILABLE_VOLUME > 4) {
-		return `LOAD fabric ${maxValueOfGoods(gameState.goodsInPort[0])}`
-	} else if (isPort(gameState) !== 'isPort' && AVAILABLE_VOLUME < TOTAL_VOLUME) {
+	wayToPort(gameState, MAP)
+
+	if (isPossibleToLoadGoods(NEW_GOODS_ARR).length > 0 && whereAmI(gameState, MAP) === 'home') {
+		return `LOAD ${loadGoodsIsPossible(isPossibleToLoadGoods(NEW_GOODS_ARR)).name} ${loadGoodsIsPossible(isPossibleToLoadGoods(NEW_GOODS_ARR)).amount}`
+	}
+	if (!isPossibleToLoadGoods(NEW_GOODS_ARR).length > 0 && whereAmI(gameState, MAP) !== 'port') {
 		return 'N'
-	} else if (isPort(gameState) === 'isPort' && AVAILABLE_VOLUME < TOTAL_VOLUME) {
-		return `SELL ${gameState.ship.goods[0].name} ${gameState.ship.goods[0].amount}`
-	} else {
+	}
+	if (isPossibleToSellGoods(gameState) && whereAmI(gameState, MAP) === 'port') {
+		return `SELL ${isPossibleToSellGoods(gameState).name} ${isPossibleToSellGoods(gameState).amount}`
+	}
+	if (!isPossibleToSellGoods(gameState) && whereAmI(gameState, MAP) !== 'home') {
 		return 'S'
 	}
-
 }
 
-function isPort(gameState) {
-	const { ship, ports } = gameState
-	if (ports.filter(({ x, y, isHome }) => x === ship.x && y === ship.y && isHome === true).length > 0) {
-		return 'isHome'
-	} else if (ports.filter(({ x, y, isHome }) => x === ship.x && y === ship.y && isHome === false).length > 0) {
-		return 'isPort'
+function setAvailableVolume(gameState) {
+	const { goodsInPort, ship } = gameState
+	const { goods } = ship
+	let availableVolumeVar = TOTAL_VOLUME
+	if (goods.length > 0) {
+		goods.forEach(el => {
+			const { volume } = goodsInPort.filter(({ name }) => name === el.name)[0]
+			const curGoodsVolume = el.amount * volume
+			availableVolumeVar = availableVolumeVar - curGoodsVolume
+		});
+		AVAILABLE_VOLUME = availableVolumeVar
+	} else {
+		AVAILABLE_VOLUME = TOTAL_VOLUME
+	}
+}
+
+function generateMap(levelMap, gameState) {
+	const { ports } = gameState
+	const fullMapArray = levelMap.split('\n')
+	const fullMapNewArray = []
+	fullMapArray.map(element => {
+		const xAxis = []
+		for (let key in element) {
+			xAxis.push({
+				x: +key,
+				type: generateMapItemType(element[key])
+			})
+		}
+		fullMapNewArray.push(xAxis)
+	})
+	ports.forEach(port => {
+		fullMapNewArray[port.y][port.x]['isHome'] = port.isHome
+		fullMapNewArray[port.y][port.x]['portId'] = port.portId
+	})
+	MAP = fullMapNewArray
+}
+
+function generateMapItemType(symbol) {
+	switch (symbol) {
+		case '~':
+			return 'water'
+		case '#':
+			return 'land'
+		case 'O':
+			return 'port'
+		case 'H':
+			return 'home'
+		default:
+			return false
+	}
+}
+
+
+function isPossibleToLoadGoods(newGoodsArr) {
+	let possibleAvailableVolume = AVAILABLE_VOLUME
+	const minVolumePerUnit = Math.min.apply(null, newGoodsArr.map(el => el.volume))
+	const someGoods = []
+
+	newGoodsArr.forEach(el => {
+		const totalAvailableVolumePerGoods = possibleAvailableVolume
+		const possibleAmountOfThisGoods = parseInt(possibleAvailableVolume / el.volume)
+
+		possibleAvailableVolume = totalAvailableVolumePerGoods - possibleAmountOfThisGoods * el.volume
+		if (possibleAmountOfThisGoods !== 0) {
+			someGoods.push({ ...el, possibleAmount: possibleAmountOfThisGoods })
+		}
+		if (possibleAvailableVolume + minVolumePerUnit < TOTAL_VOLUME) {
+			return
+		}
+	})
+	return someGoods
+}
+
+function loadGoodsIsPossible(arrOfGoods) {
+	if (arrOfGoods.length > 0) {
+		return {
+			name: arrOfGoods[0].name,
+			amount: arrOfGoods[0].possibleAmount
+		}
 	} else {
 		return false
 	}
 }
 
-function maxValueOfGoods(oneTypeOfGoods) {
-	const { volume } = oneTypeOfGoods
-	if (AVAILABLE_VOLUME % volume === 0) {
-		return AVAILABLE_VOLUME / volume - 1
+function cofficentOfBenefit(gameState) {
+	const { goodsInPort, prices } = gameState
+	const newArr = []
+	goodsInPort.forEach(goods => {
+		prices.forEach(port => {
+			newArr.push({
+				name: goods.name,
+				amount: goods.amount,
+				volume: goods.volume,
+				corfficentOfBenefit: port[goods.name] / goods.volume
+			})
+		})
+	})
+	NEW_GOODS_ARR = newArr.sort((a, b) => a.cofficentOfBenefit < b.cofficentOfBenefit ? 1 : -1)
+}
+
+function whereAmI(gameState, newMap) {
+	const { ship } = gameState
+	const curCoordinates = newMap[ship.y].filter(el => el.x === ship.x)[0]
+	return curCoordinates.type
+}
+
+function isPossibleToSellGoods(gameState) {
+	const { ship } = gameState
+	if (ship.goods.length > 0) {
+		return { ...ship.goods[0] }
 	} else {
-		return AVAILABLE_VOLUME / volume
+		return false
 	}
 }
 
-function setAvailableVolume(allData) {
-	const { goodsInPort, ship } = allData
-	const { goods } = ship
-	if (goods.length > 0) {
-		goods.forEach(el => {
-			const { volume } = goodsInPort.filter(({ name }) => name === el.name)[0]
-			AVAILABLE_VOLUME = TOTAL_VOLUME - el.amount * volume
-		});
+function nextDestination(gameState) {
+	const { ship } = gameState
+	if (ship.goods.length > 0) {
+		return 'toSellGoods'
 	} else {
-		AVAILABLE_VOLUME = TOTAL_VOLUME
+		return 'toLoadGoods'
+	}
+}
+
+function wayToPort(gameState, newMap) {
+	const { ship, ports } = gameState
+	if (nextDestination(gameState) === 'toLoadGoods') {
+		const home = ports.filter(port => port.isHome)[0]
+		direction(home.y, ship.y, 'y')
+		direction(home.x, ship.x, 'x')
+	}
+	if (nextDestination(gameState) === 'toSellGoods') {
+		const port = ports.filter(port => !port.isHome)[0]
+		direction(port.y, ship.y, 'y')
+		direction(port.x, ship.x, 'x')
+	}
+}
+
+function direction(destinationCoord, startCoord, axis) {
+	if (destinationCoord < startCoord && axis === 'y'){
+		return 'N'
+	}
+	if (destinationCoord > startCoord && axis === 'y'){
+		return 'S'
+	}
+	if (destinationCoord < startCoord && axis === 'x'){
+		return 'W'
+	}
+	if (destinationCoord < startCoord && axis === 'x'){
+		return 'E'
 	}
 }
